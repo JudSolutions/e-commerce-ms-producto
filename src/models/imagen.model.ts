5cedf13ff170d00bd529174a2c3ed9a0a3280507
JudSolutions <judsolutions13@outlook.com>
2024-04-28 18:30:34 -0500

    modelos

---
+++ src/models/imagen.model.ts
@@ -0,0 +1,33 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model({settings: {strict: false}})
+export class Imagen extends Entity {
+  @property({
+    type: 'number',
+    id: true,
+    generated: true,
+  })
+  id?: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  Nombre: string;
+
+  // Define well-known properties here
+
+  // Indexer property to allow additional data
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  [prop: string]: any;
+
+  constructor(data?: Partial<Imagen>) {
+    super(data);
+  }
+}
+
+export interface ImagenRelations {
+  // describe navigational properties here
+}
+
+export type ImagenWithRelations = Imagen & ImagenRelations;
---
+++ src/models/producto.model.ts
@@ -0,0 +1,52 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Producto extends Entity {
+  @property({
+    type: 'number',
+    id: true,
+    generated: true,
+  })
+  id?: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  Nombre: string;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  Precio: number;
+
+  @property({
+    type: 'number',
+    default: 0,
+  })
+  Existencia?: number;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  Calificacion: number;
+
+  @property({
+    type: 'number',
+    default: 0,
+  })
+  Descuento?: number;
+
+
+  constructor(data?: Partial<Producto>) {
+    super(data);
+  }
+}
+
+export interface ProductoRelations {
+  // describe navigational properties here
+}
+
+export type ProductoWithRelations = Producto & ProductoRelations;
