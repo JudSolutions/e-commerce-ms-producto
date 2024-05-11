78897b38dbe52b42829de1fa6907c3ba2cba6428
JudSolutions <judsolutions13@outlook.com>
2024-05-11 06:48:22 -0500

    controladores

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,3 +1,7 @@
 export * from './ping.controller';
 export * from './producto-marca.controller';
 export * from './marca-producto.controller';
+export * from './marca.controller';
+export * from './categoria.controller';
+export * from './imagen.controller';
+export * from './producto.controller';
